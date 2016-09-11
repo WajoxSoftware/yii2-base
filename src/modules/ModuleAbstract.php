e5527794abc553f1f85e10b6478442dbe9d6c129
Ildar Usmanov <wajox@mail.ru>
2016-09-11 20:43:21 +0300

    add module abstract

---
+++ src/modules/ModuleAbstract.php
@@ -0,0 +1,30 @@
+<?php
+namespace app\modules;
+
+abstract class ModuleAbstract extends \yii\base\Module
+{
+
+    public $hasSessionController = false;
+    public $hasRegistrationController = false;
+
+    public function init()
+    {
+        parent::init();
+        $this->initModule();
+    }
+
+    protected function initModule()
+    {
+        $this->controllerNamespace = 'app\modules\\'.$this->id.'\controllers';
+        $this->layout = 'profile';
+        \Yii::$app->user->loginUrl = ['/'.$this->id.'/session'];
+    }
+
+    public function getModuleHomeBreadcrumbs()
+    {
+        return [
+            'label' => \Yii::t('app/'.$this->id, 'Module Home'),
+            'url' => ['/'.$this->id],
+        ];
+    }
+}
