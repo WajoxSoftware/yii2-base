2c1b2581dc45cf9aeb515c80a94b4ee93a209657
Ildar Usmanov <wajox@mail.ru>
2016-12-26 15:59:19 +0300

    clean up

--- src/extensions/Bootstrap.php
+++ src/extensions/Bootstrap.php
@@ -11,6 +11,45 @@ class Bootstrap extends Component implements BootstrapInterface
 {
     const APP_BASE_THEME = 'base';
 
+    protected $i18nMap = [
+        'app/general' => 'general.php',
+        'app/notifications' => 'notifications.php',
+        'app/models' => 'models.php',
+        'app/attributes' => 'attributes.php',
+        'app/account' => 'account.php',
+        'app/admin' => 'admin.php',
+        'app/dialogs' => 'dialogs.php',
+        'app/partner' => 'partner.php',
+        'app/payment' => 'payment.php',
+        'app/profile' => 'profile.php',
+        'app/shop' => 'shop.php',
+        'app/trafficmanager' => 'trafficmanager.php',
+        'app/validation' => 'validation.php',
+        'app/error' => 'error.php',
+    ];
+
+    protected $commandsMap = [
+        'good-letter-emails' => 'wajox\yii2base\commands\GoodLetterEmailsController',
+        'mailer' => 'wajox\yii2base\commands\MailerController',
+        'orders' => 'wajox\yii2base\commands\OrdersController',
+        'partners' => 'wajox\yii2base\commands\PartnersController',
+        'rbac' => 'wajox\yii2base\commands\RbacController',
+        'sender-settings' => 'wajox\yii2base\commands\SenderSettingsController',
+        'settings' => 'wajox\yii2base\commands\SettingsController',
+        'uploaded-files' => 'wajox\yii2base\commands\UploadedFilesController',
+        'user-notifications' => 'wajox\yii2base\commands\UserNotificationsController',
+        'users' => 'wajox\yii2base\commands\UsersController',
+    ];
+
+    protected $controllersMap = [
+        'site' => 'wajox\yii2base\controllers\SiteController',
+        'confirmation' => 'wajox\yii2base\controllers\ConfirmationController',
+        'content-nodes' => 'wajox\yii2base\controllers\ContentNodesController',
+        'password' => 'wajox\yii2base\controllers\PasswordController',
+        'subscribes' => 'wajox\yii2base\controllers\SubscribesController',
+        'traffic-streams' => 'wajox\yii2base\controllers\TrafficStreamsController',
+    ];
+
     public function bootstrap($app)
     {
         $this->initAppSettings($app);
@@ -93,13 +132,6 @@ class Bootstrap extends Component implements BootstrapInterface
         $app->urlManager->addRules(['/' => $indexUrl], false);
     }
 
-    protected function add($model)
-    {
-        $this->items[$model->key] = $model;
-
-        return $this;
-    }
-
     protected function initI18n($app)
     {
         if (isset($app->i18n->translations['app*'])) {
@@ -109,51 +141,17 @@ class Bootstrap extends Component implements BootstrapInterface
         $app->i18n->translations['app*'] = [
             'class'    => PhpMessageSource::className(),
             'basePath' => __DIR__ . '/../messages',
-            'fileMap' => [
-                'app/general' => 'general.php',
-                'app/notifications' => 'notifications.php',
-                'app/models' => 'models.php',
-                'app/attributes' => 'attributes.php',
-                'app/account' => 'account.php',
-                'app/admin' => 'admin.php',
-                'app/dialogs' => 'dialogs.php',
-                'app/partner' => 'partner.php',
-                'app/payment' => 'payment.php',
-                'app/profile' => 'profile.php',
-                'app/shop' => 'shop.php',
-                'app/trafficmanager' => 'trafficmanager.php',
-                'app/validation' => 'validation.php',
-                'app/error' => 'error.php',
-            ],
+            'fileMap' => $this->i18nMap,
         ];  
     }
 
     protected function initControllersMap($app)
     {
         if ($app instanceof ConsoleApplication) {
-            $app->controllerMap = [
-                'good-letter-emails' => 'wajox\yii2base\commands\GoodLetterEmailsController',
-                'mailer' => 'wajox\yii2base\commands\MailerController',
-                'orders' => 'wajox\yii2base\commands\OrdersController',
-                'partners' => 'wajox\yii2base\commands\PartnersController',
-                'rbac' => 'wajox\yii2base\commands\RbacController',
-                'sender-settings' => 'wajox\yii2base\commands\SenderSettingsController',
-                'settings' => 'wajox\yii2base\commands\SettingsController',
-                'uploaded-files' => 'wajox\yii2base\commands\UploadedFilesController',
-                'user-notifications' => 'wajox\yii2base\commands\UserNotificationsController',
-                'users' => 'wajox\yii2base\commands\UsersController',
-            ];
-
+            $app->controllerMap = $this->commandsMap;
             return;
         }
 
-        $app->controllerMap = [
-            'site' => 'wajox\yii2base\controllers\SiteController',
-            'confirmation' => 'wajox\yii2base\controllers\ConfirmationController',
-            'content-nodes' => 'wajox\yii2base\controllers\ContentNodesController',
-            'password' => 'wajox\yii2base\controllers\PasswordController',
-            'subscribes' => 'wajox\yii2base\controllers\SubscribesController',
-            'traffic-streams' => 'wajox\yii2base\controllers\TrafficStreamsController',
-        ];
+        $app->controllerMap = $this->controllersMap;
     }
 }
