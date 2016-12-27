f29dbe58f76be2c81418bacbb453a0dd141a23fa
Ildar Usmanov <wajox@mail.ru>
2016-12-27 15:21:34 +0300

    fix

--- src/modules/payment/Payment.php
+++ src/modules/payment/Payment.php
@@ -3,9 +3,4 @@ namespace wajox\yii2base\modules\payment;
 
 class Payment extends \wajox\yii2base\modules\ModuleAbstract
 {
-    protected function initModule()
-    {
-        parent::initModule();
-        $this->layout = 'main';
-    }
 }
--- src/modules/shop/Shop.php
+++ src/modules/shop/Shop.php
@@ -1,12 +1,6 @@
 <?php
-
 namespace wajox\yii2base\modules\shop;
 
 class Shop extends \wajox\yii2base\modules\ModuleAbstract
 {
-    protected function initModule()
-    {
-        parent::initModule();
-        $this->layout = 'main';
-    }
 }
