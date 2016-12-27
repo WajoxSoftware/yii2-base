cdc9d7c0adaef666eabaf064c0aafec35d673d32
Ildar Usmanov <wajox@mail.ru>
2016-12-27 14:29:45 +0300

    move content module into ext package

--- src/modules/views/admin/nodes/_modal.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo \wajox\yii2widgets\modalformwidget\ModalFormWidget::widget([
-  'id' => 'content-node-modal',
-  'title' => $modal_title,
-  'body' => $this->render('_form_js', ['model' => $model]),
-]);
