e274f96c5446b563e7e39ca23d52100efadaddc2
Ildar Usmanov <wajox@mail.ru>
2017-04-07 11:19:13 +0300

    fix

--- src/themes/base/assets/ProfileAsset.php
+++ src/themes/base/assets/ProfileAsset.php
@@ -8,14 +8,14 @@ class ProfileAsset extends AssetBundle
     public $sourcePath = '@wajox/yii2base/themes/base/assets-src';
 
     public $css = [
-        'css/messages.css',
-        'css/bills.css',
-        'css/profile.css',
-        'css/users.css',
+        //'css/messages.css',
+        //'css/bills.css',
+        //'css/profile.css',
+        //'css/users.css',
     ];
 
     public $js = [
-        'js/sidebar.js',
+        //'js/sidebar.js',
     ];
 
     public $depends = [
