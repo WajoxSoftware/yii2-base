eb25b5e98a84338650dffe56a5494851a782640c
Ildar Usmanov <wajox@mail.ru>
2017-04-26 09:41:00 +0300

    fix

--- src/themes/base/modules/traffic/views/shared/_streams_list.php
+++ src/themes/base/modules/traffic/views/shared/_streams_list.php
@@ -1,17 +1,19 @@
-<?php foreach ($streams[$parentId] as $stream):?>
-    <ul class="collection">
-        <?= $this->render(
-            '@app/modules/traffic/views/shared/_stream_item',
-            ['model' => $stream]
-        ) ?>
+<?php if (isset($streams[$parentId])): ?>
+    <?php foreach ($streams[$parentId] as $stream):?>
+        <ul class="collection">
+            <?= $this->render(
+                '@app/modules/traffic/views/shared/_stream_item',
+                ['model' => $stream]
+            ) ?>
 
-        <?php if (isset($streams[$stream->id])): ?>
-            <ul class="collection">
-                <?= $this->render('_streams_list', [
-                    'streams' => $streams,
-                    'parentId' => $stream->id,
-                ]) ?>
-            </ul>
-        <?php endif; ?>
-    </ul>
-<?php endforeach; ?>
+            <?php if (isset($streams[$stream->id])): ?>
+                <ul class="collection">
+                    <?= $this->render('_streams_list', [
+                        'streams' => $streams,
+                        'parentId' => $stream->id,
+                    ]) ?>
+                </ul>
+            <?php endif; ?>
+        </ul>
+    <?php endforeach; ?>
+<?php endif; ?>
