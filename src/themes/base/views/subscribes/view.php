799754ad3c23445ff9948c5d8183a6e903a79c26
Ildar Usmanov <wajox@mail.ru>
2017-04-26 10:24:48 +0300

    fix

--- src/themes/base/views/subscribes/view.php
+++ src/themes/base/views/subscribes/view.php
@@ -5,7 +5,6 @@ $this->title = $emailList->title;
 <div class="row">
   <div class="col m12">
     <h1 class="text-muted text-center"><?= $emailList->title ?></h1>
-    <h6 class="text-lead text-center"><?= $emailList->description ?></h6>
   </div>
 </div>
 
