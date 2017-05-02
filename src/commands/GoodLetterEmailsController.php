6d4cd0df2cc1963c228af867042c1bbf5635f0aa
Ildar Usmanov <wajox@mail.ru>
2017-05-02 12:10:16 +0300

    fix namespace

--- src/commands/GoodLetterEmailsController.php
+++ src/commands/GoodLetterEmailsController.php
@@ -2,7 +2,7 @@
 namespace wajox\yii2base\commands;
 
 use yii\console\Controller;
-use wajox\yii2base\services\shop\GoodLetterEmailsSender;
+use wajox\yii2base\modules\shop\services\GoodLetterEmailsSender;
 
 class GoodLetterEmailsController extends Controller
 {
