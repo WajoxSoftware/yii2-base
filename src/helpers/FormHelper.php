3ceec214e85d8dd6a37a2161c83fdf711dd0395f
Ildar Usmanov <wajox@mail.ru>
2017-05-30 14:28:41 +0300

    update styles

--- src/helpers/FormHelper.php
+++ src/helpers/FormHelper.php
@@ -5,7 +5,7 @@ class FormHelper
 {
     public static function renderRurPriceField($form, $model, $field)
     {
-        $tpl = '<table class="unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td>{input}</td><td>P</td></tr></table>';
+        $tpl = '<table class="table unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td>{input}</td><td><div class="field-postfix">P</div></td></tr></table>';
 
         return $form->field($model, $field, ['inputTemplate' => $tpl]);
     }
@@ -14,14 +14,14 @@ class FormHelper
     {
         $url = \yii\helpers\Url::toRoute($routeParams, true);
 
-        $tpl = '<table class="unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td>' . $url . '</td><td width="100%">{input}</td></tr></table>';
+        $tpl = '<table class="table unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td nowrap><div class="field-prefix">' . $url . '</div></td><td width="100%">{input}</td></tr></table>';
 
         return $form->field($model, $field, ['inputTemplate' => $tpl]);
     }
 
     public static function renderPrefixField($form, $model, $field, $prefix)
     {
-        $tpl = '<table class="unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td>' . $prefix . '</td><td width="100%">{input}</td></tr></table>';
+        $tpl = '<table class="table unstyled" border="0" cellpadding="0" cellspacing="0"><tr><td nowrap><div class="field-prefix">' . $prefix . '</div></td><td width="100%">{input}</td></tr></table>';
 
         return $form->field($model, $field, ['inputTemplate' => $tpl]);
     }
