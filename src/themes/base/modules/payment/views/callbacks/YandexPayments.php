e99dac34b4c1ad1012a25bff0cafec746407b3ef
Ildar Usmanov <wajox@mail.ru>
2017-06-22 11:23:37 +0300

    fix

--- src/themes/base/modules/payment/views/callbacks/YandexPayments.php
+++ src/themes/base/modules/payment/views/callbacks/YandexPayments.php
@@ -1,6 +1,6 @@
 <?php if (in_array($action, ['process', 'checkOrder', 'paymentAviso'])): ?>
 <?php die($message) ?>
-<?php elseif ($action == 'success'): ?>
+<?php elseif (in_array($action, ['success', 'PaymentSuccess'])): ?>
   <?= $this->render('@app/modules/payment/views/shared/results/_success') ?>
 <?php else: ?>
   <?= $this->render('@app/modules/payment/views/shared/results/_fail') ?>
