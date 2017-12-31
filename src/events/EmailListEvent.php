289c0860130c9b6b2d6a88430c17b1e42c9398d6
Ildar Usmanov <wajox@mail.ru>
2018-01-01 02:24:46 +0300

    add comment

--- src/events/EmailListEvent.php
+++ src/events/EmailListEvent.php
@@ -3,6 +3,9 @@ namespace wajox\yii2base\events;
 
 use yii\base\Event;
 
+/**
+ * Event class for EmailList class
+ */
 class EmailListEvent extends Event
 {
     const EVENT_SUBSCRIBE = 'subscribed';
